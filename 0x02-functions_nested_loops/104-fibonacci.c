3147b30a0b6bd051b71d0801c46628bd81bf5eba
Daezeo <samdeborah56@gmail.com>
2022-09-14 18:16:32 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -37,7 +37,7 @@ int main(void)
 	while (i <= 98)
 	{
 		if (flo > 0)
-			printf("%lu", f1o);
+			printf("%lu", f2o);
 		initial0s = numLength(mx) - 1 - numLength(f1);
 		while (f1o > 0 && initial0s > 0)
 		{
