37f3ebf96a5fb2e4c48cb3e599476893fdf31a0f
Daezeo <samdeborah56@gmail.com>
2022-09-16 12:01:22 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,12 +8,11 @@
 
 void print_numbers(void)
 {
-	char a = 0;
+	int num = 0;
 
-	while (a <= 9)
-	{
-		_putchar(a + '0');
-		a++;
-	}
+	do {
+		_putchar(num + 48);
+		num++;
+	} while (num >= 0 && num <= 9);
 	_putchar('\n');
 }
